aea8eb00606b5af3521c8859f3ec544524bcad93
Amanda Tsai <amanda.tsai11@gmail.com>
2026-02-07 01:25:32 -0800

    update

--- scripts/supplementals_format.py
+++ scripts/supplementals_format.py
@@ -6,3 +6,14 @@ import re
 script_dir = Path(__file__).parent
 input_dir = script_dir / "../data/organized_data/supplementals/"
 output_path = script_dir / "../data/finalized_data_json/supplementals.jsonl"
+
+def main():
+
+
+
+
+
+
+    
+if __name__ == "__main__":
+    main()

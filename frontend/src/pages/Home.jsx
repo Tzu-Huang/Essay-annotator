e3af4922aac41b2ef02c22f0fce3374bfc623d0d
Olivia Chu <liv88.chu@gmail.com>
2026-03-07 10:48:03 -0600

    update homepage and add logo

---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,46 @@
+import { Link } from "react-router-dom";
+
+function Home() {
+  return (
+    <div className="home">
+
+      <div className="hero">
+        <h1>
+          Learn from Real Accepted Essays — Not just AI Opinions
+        </h1>
+
+        <p>
+          Upload your essay and discover similar essays
+          that got into top U.S. universities.
+        </p>
+
+        <Link to="/login">
+          <button className="primary-btn">
+            start writing
+          </button>
+        </Link>
+      </div>
+
+      <div className="how-it-works">
+        <h2>How it works</h2>
+
+        <div className="steps">
+          <div className="step">
+            <p>upload your draft</p>
+          </div>
+
+          <div className="step">
+            <p>get top-k similar essays</p>
+          </div>
+
+          <div className="step">
+            <p>compare structure & improve</p>
+          </div>
+        </div>
+      </div>
+
+    </div>
+  );
+}
+
+export default Home;

a2e9683d65b43eb788f69266e766c51cc59fcdf6
Olivia Chu <liv88.chu@gmail.com>
2026-03-20 16:29:12 -0500

    front change of out put result

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,16 +6,26 @@ import Editor from "./pages/Editor";
 import "./App.css";   
 
 function App() {
+
+  const bg = "bg-world"; 
+  // 改成 bg-us 就會換圖
+
   return (
     <BrowserRouter>
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/editor" element={<Editor />} />
-      </Routes>
+
+      <div className={`background-image ${bg}`}>
+
+        <Navbar />
+
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/editor" element={<Editor />} />
+        </Routes>
+
+      </div>
+
     </BrowserRouter>
   );
 }
-
 export default App;

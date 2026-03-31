215c8bfdfc3695f57a816dde84b85a6db52174a6
Olivia Chu <liv88.chu@gmail.com>
2026-03-31 11:47:20 -0700

    add essay page and update routing

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,24 +3,24 @@ import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Editor from "./pages/Editor";
+import EssayPage from "./pages/EssayPage";
 import "./App.css";   
 
 function App() {
 
   const bg = "bg-world"; 
-  // 改成 bg-us 就會換圖
 
   return (
     <BrowserRouter>
 
       <div className={`background-image ${bg}`}>
 
-        <Navbar />
-
         <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/editor" element={<Editor />} />
+          <Route path="/" element={<><Navbar /><Home /></>} />
+          <Route path="/login" element={<><Navbar /><Login /></>} />
+          <Route path="/editor" element={<><Navbar /><Editor /></>} />
+          <Route path="/essay/:id" element={<EssayPage />} />
+
         </Routes>
 
       </div>
@@ -28,4 +28,5 @@ function App() {
     </BrowserRouter>
   );
 }
+
 export default App;

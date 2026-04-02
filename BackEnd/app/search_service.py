c7fb41d540578511001c7deece5017d7d73ed30c
Amanda Tsai <amanda.tsai11@gmail.com>
2026-04-02 12:05:22 -0700

    list

--- BackEnd/app/search_service.py
+++ BackEnd/app/search_service.py
@@ -103,7 +103,7 @@ def run_search(app_state, topK: int, essay_type: list, topic: str, content: str)
         else:
             normalized_inputs = [normalized_essay_type(e) for e in essay_type]
             for idx, t in enumerate(types):
-                if normalized_essay_type(t) == normalized_inputs:
+                if normalized_essay_type(t) in normalized_inputs:
                     allowed_idx.append(idx)
 
     print("yaya")

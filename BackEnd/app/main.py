fff9093d68e36466b60782800b857395a5f8f2fc
Amanda Tsai <139444765+AmandaTsai24@users.noreply.github.com>
2026-05-19 00:41:26 -0700

    fix

--- BackEnd/app/main.py
+++ BackEnd/app/main.py
@@ -6,9 +6,12 @@ from service.search_service import run_search
 from app.state import AppData
 from compare_results.analysis import compare
 from dotenv import load_dotenv
-from embedding.search_similar import load_db_embeddings
+from embedding.search_similar import 
+from database.create import get_db, User, create_tables
 from fastapi import FastAPI, HTTPException, Query, Request
 from fastapi.middleware.cors import CORSMiddleware
+from sqlalchemy.orm import Session
+from datetime import datetime, timezone
 from pathlib import Path
 from pydantic import BaseModel
 from typing import Optional
